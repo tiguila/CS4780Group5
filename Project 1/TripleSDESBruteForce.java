8cebe85fc300da25a5e380e6f361ff57e66c7f70
tiguila <102114848+tiguila@users.noreply.github.com>
2023-10-27 18:16:13 -0700

    Update TripleSDESBruteForce.java

--- Project 1/TripleSDESBruteForce.java
+++ Project 1/TripleSDESBruteForce.java
@@ -2,42 +2,28 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-/** Note: Do NOT SHARE ANY OF THE CODE HERE WITH OTHERS.
+/** 
  * Project 1, Part 3.3
  * Triple SDES brute force decryption
- * 
- * 
- * You amy change the range in range() or 
- * bruteForceIterations and bruteForceLowerBound variable values.
  */
 public class TripleSDESBruteForce {
 
     public static BruteForceOperations bfo = new BruteForceOperations();
     public static Map<Integer, byte[]> dictionary = bfo.dictionaryKeys();
     
-    /** 
-    // left here at index 26563
-    **** CONTINUE FROM index 26563 TO 524800
-    private static int bruteForceIterations = 0; // limit is 524800, upper bound
-    private static int bruteForceLowerBound = 0; // lower bound
-    */
-
-    // start: 400,000
-    private static int bruteForceIterations = 0; // limit is 524800, upper bound
-    private static int bruteForceLowerBound = 0; // lower bound
+    private static int bruteForceIterations = 922969; // limit is 2^20, upper bound
+    private static int bruteForceLowerBound = 922969; // lower bound
 
     public static void main(String[] args) {
         
-        long startTime = System.nanoTime();
-        System.out.println(bruteForceIterations);
+        // System.out.println("In progress...");
 
-        // String text = "00011111100111111110011111101100111000000011001011110010101010110001011101001101000000110011010111111110000000001010111111000001010010111001111001010101100000110111100011111101011100100100010101000011001100101000000101111011000010011010111100010001001000100001111100100000001000000001101101000000001010111010000001000010011100101111001101111011001001010001100010100000";
-        String text  = "0001111110011111111001111110110011100000001100101111001010101011000101110100110100000011001101011111111000000000101011111100000101001011100111100101010110000011";
+        String text  = "00011111100111111110011111101100111000000011001011110010101010110001011101001101000000110011010111111110000000001010111111000001010010111001111001010101100000110111100011111101011100100100010101000011001100101000000101111011000010011010111100010001001000100001111100100000001000000001101101000000001010111010000001000010011100101111001101111011001001010001100010100000";
 
-        
         int[] temp = range();
         for(int i=0; i<temp.length; i++) {
             bruteForceLowerBound = bruteForceIterations;
@@ -45,25 +31,26 @@ public class TripleSDESBruteForce {
             bruteForceDecrypt(text);
         }
 
-        long endTime = System.nanoTime();
+        System.out.println("Process completed.");
 
-        long elapsedTime = endTime - startTime;
-
-        System.out.println("Time taken: " + ((elapsedTime / 1e6)/1000)/60 + " sec"); // Convert to milliseconds
+        // printResults();
 
     }
 
-    // do interations in bursts of 5, repeat this for 1000
-    public static int[] range() {
-        int[] x = new int[1000];
-        for(int i=0; i<x.length; i++) {
-            x[i] = 5;
-        }
-        return x;
+    public static  void printResults() {
+
+        int index = 922979;
+        List<List<Integer>> product = findCrossProduct();
+        List<Integer> targetPair = product.get(index);
+        byte[] key1 = dictionary.get(targetPair.get(0));
+        byte[] key2 = dictionary.get(targetPair.get(1));
+        String plaintext = "THERE ARE NO SECRETS BETTER KEPT THAN THE SECRETS THAT EVERYBODY GUESSES.";
+        System.out.println("Key 1: " + Arrays.toString(key1));
+        System.out.println("Key 2: " + Arrays.toString(key2));
+        System.out.println("Key 1: " + Arrays.toString(key1));
+        System.out.println(plaintext);
+
     }
-
-
-
 
 
     /**
@@ -71,10 +58,10 @@ public class TripleSDESBruteForce {
      * @param text The text to decrypt
      */
     public static void bruteForceDecrypt(String text) {
-        
+
         try {
             // Create a BufferedWriter to write to the file, append any previous content of output.txt to the current file.
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("tripleSDESBruteForce.txt", true));
 
             for(int i=bruteForceLowerBound; i<bruteForceIterations; i++) {
                 String temp = CASCII.toString(tripleSDESBruteForce(text)[i]);
@@ -85,9 +72,6 @@ public class TripleSDESBruteForce {
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            System.out.println("Process Completed!");
         }
     }
 
@@ -142,7 +126,7 @@ public class TripleSDESBruteForce {
         List<List<Integer>> crossProduct = new ArrayList<>();
         
         for (int i = 0; i < a.length; i++) {
-            for (int j = i; j < a.length; j++) {
+            for (int j = 0; j < a.length; j++) {
                 List<Integer> pair = new ArrayList<>();
                 pair.add(a[i]);
                 pair.add(a[j]);
@@ -151,5 +135,16 @@ public class TripleSDESBruteForce {
         }
         return crossProduct;
     }
+
+
+    // do interations in bursts of 2, repeat this for 5
+    public static int[] range() {
+        int[] x = new int[5];
+        for(int i=0; i<x.length; i++) {
+            x[i] = 3;
+        }
+        return x;
+    }
+
    
 }
